7dcb88e32ed527609c0d20689c8e1e4dd603ed1a
Garry Blight <garry.blight@guardian.co.uk>
2015-06-17 10:28:01 +0100

    update

--- src/app/js/main.js
+++ src/app/js/main.js
@@ -1,36 +1,17 @@
-var Ractive = require('ractive');
-var getJSON = require('./utils/getjson'); 
-var app;
-
-/**
- * Update app using fetched JSON data
- * @param {object:json} data - JSON spreedsheet data.
- */
-function updateView(data) {
-	app.set('games', data.sheets.games);
-}
-
-
 /**
  * Boot the app.
  * @param {object:dom} el - <figure> element passed by boot.js. 
  */
 function boot(el) {
-	app = new Ractive( {
-	    el: el,
-	    template: require('./templates/base.html'),
-	    data: {
-			games: require('./data/data.json')
-		},
-		components: {
-			subView: require('./subView'),
-			socialButtons: require('./components/socialButtons')
-		}
-	});
-	
-	var key = '1hy65wVx-pjwjSt2ZK7y4pRDlX9wMXFQbwKN0v3XgtXM';
-	var url = '//visuals.guim.co.uk/spreadsheetdata/'+key+'.json';
-	getJSON(url, updateView);
+	var html = require('./templates/index.html');
+	el.innerHTML = html;
+
+	var  p = el.querySelector('p');
+	p.addEventListener('click', function() {
+		console.log('clicked');
+	}, false);
+
+	console.log(p);
 }
 
 // AMD define for boot.js
